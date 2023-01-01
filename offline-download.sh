454ef1b25322719fc629797c0968689a52924445
PhilipZubel <philip.zubel1@gmail.com>
2023-01-01 22:27:33 +0000

    refactored transfer script

--- offline-download.sh
+++ offline-download.sh
@@ -1,55 +1,127 @@
 #!/bin/bash
 
-# Include a offline-version.txt file to decide which offline storage version to download.
-# In order to include the current version type `main` inside offline-version.txt
-
-CONFIG=${PWD}/offline-version.txt
 BUCKET_NAME=gs://task-search-bucket
 
-INDEX_PATH="indexes"
-TASK_DATASETS_PATH="task_datasets"
+# $1 - local folder name
+# $2 - name of zipped file
+download () {
 
-
-if [[ ! -f $CONFIG ]] 
-then
-    echo "File $CONFIG does not exist."
-    echo "This file should contain the snapshot id of the version you want to download."
-    echo "To pull the main version, write 'main' inside offline-version.txt."
+    # Check if remote file exists
+    REMOTE_PATH=$BUCKET_NAME/$2.zip
+    gsutil -q stat $REMOTE_PATH
+    status=$?
+    if [[ $status == 1 ]]; then
+    echo "Remote file $REMOTE_PATH does not exist..."
     exit 1
-fi
-
-SNAPSHOT_ID=`cat ${CONFIG}`
-echo "Snapshot ID: $SNAPSHOT_ID"
-
-REMOTE_VERSIONS=`gsutil ls "${BUCKET_NAME}/offline/"`
-
-REMOTE_EXISTS=false
-for VERSION in $REMOTE_VERSIONS
-do
-    if [[ "$VERSION" == *"$SNAPSHOT_ID"* ]]; then
-        REMOTE_EXISTS=true
     fi
+    
+    # Download zipped folder and delete local folder if exists
+    LOCAL_PATH=$PWD/$1
+    mkdir=$PWD/test
+    if [ -d "$LOCAL_PATH" ]; then
+        echo "Are you sure you want to delete ${LOCAL_PATH}? [Y/n]"
+        read -r -p "Are you sure? [y/N] " response
+        case "$response" in
+            [yY][eE][sS]|[yY]) 
+                gsutil -m cp $BUCKET_NAME/$2.zip $PWD/$2.zip
+                echo "Deleting local offline directory..."
+                rm -r $LOCAL_PATH
+                ;;
+            *)
+                echo "Exiting script..."
+                exit 0
+                ;;
+        esac
+    else
+        gsutil -m cp $BUCKET_NAME/$2.zip $PWD/$2.zip
+    fi
+
+    echo "Unzipping $PWD/$2/ to $LOCAL_PATH "
+    unzip ${PWD}/$2.zip -d $PWD/test/
+
+    # Remove zipped file
+    rm ${PWD}/$2.zip
+
+}
+
+while getopts 'idta' OPTION; do
+  case "$OPTION" in
+    i)
+      echo "Downloading remote indexes..."
+      download indexes indexes
+      echo "Successfully downloaded indexes!"
+      exit 0
+      ;;
+    d)
+      echo "Downloading remote datasets..."
+      download task_datasets task_datasets
+      echo "Successfully downloaded datasets!"
+
+      exit 0
+      ;;
+    t)
+      echo "Downloading remote taskgraph objects..."
+      download bin taskgraphs 
+      echo "Successfully downloaded taskgraphs!"
+      exit 0
+      ;;
+      
+    a)
+      echo "Downloading remote indexes, remote datasets, and remote taskgraph objects..."
+      download indexes indexes
+      download task_datasets task_datasets
+      download bin taskgraphs
+      echo "Successfully downloaded artefacts!"
+      exit 0
+      ;;
+    ?)
+      echo "script usage: $(basename \$0) [-i] [-d] [-a]" >&2
+      exit 1
+      ;;
+  esac
 done
-if [ $REMOTE_EXISTS = false ]; then
-    echo "Could not find remote snapchot with ID: $SNAPSHOT_ID"
-    exit 1
-fi
+shift "$(($OPTIND -1))"
 
-echo "Are you sure you want to delete ${LOCAL_FOLDER_PATH}? [Y/n]"
-read -r -p "Are you sure? [y/N] " response
-case "$response" in
-    [yY][eE][sS]|[yY]) 
-        echo "Deleting local offline directory..."
-        rm -r ${PWD}/${INDEX_PATH}
-        rm -r ${PWD}/${TASK_DATASETS_PATH}
-        ;;
-    *)
-        exit 0
-        ;;
-esac
 
-echo "Downloading files..."
-gsutil -m cp ${BUCKET_NAME}/offline/${SNAPSHOT_ID}*/offline.zip ${PWD}/offline.zip
-unzip ${PWD}/offline.zip -d ${PWD}
-rm ${PWD}/offline.zip
-echo "Download successful!"
+# CONFIG=${PWD}/offline-version.txt
+# BUCKET_NAME=gs://task-search-bucket
+
+# INDEX_PATH="indexes"
+# TASK_DATASETS_PATH="task_datasets"
+
+
+# SNAPSHOT_ID=`cat ${CONFIG}`
+# echo "Snapshot ID: $SNAPSHOT_ID"
+
+# REMOTE_VERSIONS=`gsutil ls "${BUCKET_NAME}/offline/"`
+
+# REMOTE_EXISTS=false
+# for VERSION in $REMOTE_VERSIONS
+# do
+#     if [[ "$VERSION" == *"$SNAPSHOT_ID"* ]]; then
+#         REMOTE_EXISTS=true
+#     fi
+# done
+# if [ $REMOTE_EXISTS = false ]; then
+#     echo "Could not find remote snapchot with ID: $SNAPSHOT_ID"
+#     exit 1
+# fi
+
+# echo "Are you sure you want to delete ${LOCAL_FOLDER_PATH}? [Y/n]"
+# read -r -p "Are you sure? [y/N] " response
+# case "$response" in
+#     [yY][eE][sS]|[yY]) 
+#         echo "Deleting local offline directory..."
+#         rm -r ${PWD}/${INDEX_PATH}
+#         rm -r ${PWD}/${TASK_DATASETS_PATH}
+#         ;;
+#     *)
+#         exit 0
+#         ;;
+# esac
+
+# echo "Downloading files..."
+# gsutil -m cp ${BUCKET_NAME}/offline/${SNAPSHOT_ID}*/offline.zip ${PWD}/offline.zip
+# unzip ${PWD}/offline.zip -d ${PWD}
+# rm ${PWD}/offline.zip
+# echo "Download successful!"
---
+++ transfer-remote.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+# Run ./offline-upload to save repository artefacts
+
+# -i arg - flag stores indexes/ in the offline bucket
+# -d arg - flag stores task_datasets/ in the offline bucket
+# -t arg - flag stores bin/ in the offline bucket (taskgraph objects)
+# -a arg - flag stores all artefacts in the offline bucket
+# arg - either keyword upload or download
+
+BUCKET_NAME=gs://task-search-bucket
+
+# $1 - local folder name
+# $2 - name of zipped file
+upload () {
+  echo "UPLOAD started!"
+  # Zip contents
+  echo "Zipping ${PWD}/$1/"
+  zip -r $2.zip $1/
+  
+  # Remove folder in offline storage
+  REMOTE_PATH=$BUCKET_NAME/$2.zip
+  gsutil -q stat $REMOTE_PATH
+  status=$?
+  if [[ $status == 0 ]]; then
+    echo "Removing $REMOTE_PATH..."
+    gsutil -q rm -r $REMOTE_PATH
+  fi
+
+  echo "Uploading ${PWD}/$1/ to $REMOTE_PATH"
+  gsutil -m cp -Z $PWD/$2.zip $REMOTE_PATH
+  
+  # Delete zipped folder
+  rm $2.zip
+}
+
+download () {
+  echo "Downlaod started!"
+  # Check if remote file exists
+  REMOTE_PATH=$BUCKET_NAME/$2.zip
+  gsutil -q stat $REMOTE_PATH
+  status=$?
+  if [[ $status == 1 ]]; then
+  echo "Remote file $REMOTE_PATH does not exist..."
+  exit 1
+  fi
+  
+  # Download zipped folder and delete local folder if exists
+  LOCAL_PATH=$PWD/$1
+  mkdir=$PWD/test
+  if [ -d "$LOCAL_PATH" ]; then
+      echo "Are you sure you want to delete ${LOCAL_PATH}? [Y/n]"
+      read -r -p "Are you sure? [y/N] " response
+      case "$response" in
+          [yY][eE][sS]|[yY]) 
+              gsutil -m cp $BUCKET_NAME/$2.zip $PWD/$2.zip
+              echo "Deleting local offline directory..."
+              rm -r $LOCAL_PATH
+              ;;
+          *)
+              echo "Exiting script..."
+              exit 0
+              ;;
+      esac
+  else
+      gsutil -m cp $BUCKET_NAME/$2.zip $PWD/$2.zip
+  fi
+
+  echo "Unzipping $PWD/$2/ to $LOCAL_PATH "
+  unzip ${PWD}/$2.zip -d $PWD/test/
+
+  # Remove zipped file
+  rm ${PWD}/$2.zip
+}
+
+transfer () {
+  if [[ $3 == "upload" ]]; then
+    upload $1 $2
+  elif [[ $3 == "download" ]]; then
+    download $1 $2
+  else
+    echo "$3 not a valid argument. Either enter upload or download."
+    exit 1
+  fi
+}
+
+
+while getopts 'i:d:t:a:' OPTION; do
+  case "$OPTION" in
+    i)
+      echo "Transferring local indexes..."
+      transfer indexes indexes ${OPTARG}
+      echo "Successfully transfered indexes!"
+      exit 0
+      ;;
+    d)
+      echo "Transferring local datasets..."
+      transfer task_datasets task_datasets ${OPTARG}
+      echo "Successfully transfered datasets!"
+      exit 0
+      ;;
+    t)
+      echo "Transferring local taskgraph objects..."
+      transfer bin taskgraphs ${OPTARG}
+      echo "Successfully transfered taskgraphs!"
+      exit 0
+      ;;
+      
+    a)
+      echo "Transferring local indexes, local datasets, and local taskgraph objects..."
+      transfer indexes indexes ${OPTARG}
+      transfer task_datasets task_datasets ${OPTARG}
+      transfer bin taskgraphs ${OPTARG}
+      echo "Successfully transfered artefacts!"
+      exit 0
+      ;;
+    ?)
+      echo "script usage: $(basename \$0) [-i] [-d] [-a]" >&2
+      exit 1
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
